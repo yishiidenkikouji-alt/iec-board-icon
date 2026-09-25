73f8c81c5f903e4826cb8bfa9068ac42a9984e34
yishiidenkikouji-alt <y.ishiidenkikouji@gmail.com>
2026-09-25 18:01:06 +0900

    Bump PWA cache for general user recovery

--- board-pwa/sw.js
+++ board-pwa/sw.js
@@ -5,7 +5,7 @@
 'use strict';
 const ROOT=new URL('./',self.location.href);
 const PREFIX='iec-board-pwa-shell:'+ROOT.pathname+':';
-const CACHE=PREFIX+'v29-android-settings-recovery';
+const CACHE=PREFIX+'v30-general-user-recovery';
 const FILES=['./','./index.html','./styles.css','./config.js','./app.js','./entry-session.js','./settings-menu.js','./manifest.webmanifest'];
 const PUBLIC_URLS=FILES.map(path=>new URL(path,ROOT).href);
 self.addEventListener('install',event=>{
