18b8a4091ab7df1538c1a6877a45c3517b6f4beb
yishiidenkikouji-alt <y.ishiidenkikouji@gmail.com>
2026-09-25 18:06:18 +0900

    Bump cache for slow GAS startup fix

--- board-pwa/sw.js
+++ board-pwa/sw.js
@@ -5,7 +5,7 @@
 'use strict';
 const ROOT=new URL('./',self.location.href);
 const PREFIX='iec-board-pwa-shell:'+ROOT.pathname+':';
-const CACHE=PREFIX+'v30-general-user-recovery';
+const CACHE=PREFIX+'v31-slow-gas-startup';
 const FILES=['./','./index.html','./styles.css','./config.js','./app.js','./entry-session.js','./settings-menu.js','./manifest.webmanifest'];
 const PUBLIC_URLS=FILES.map(path=>new URL(path,ROOT).href);
 self.addEventListener('install',event=>{
