71c86dc3573b6e2ad37c4da079285b4db124f5bd
yishiidenkikouji-alt <y.ishiidenkikouji@gmail.com>
2026-09-25 18:10:13 +0900

    Rollback PWA shell to last known working 2026-09-24 build

--- board-pwa/sw.js
+++ board-pwa/sw.js
@@ -5,8 +5,8 @@
 'use strict';
 const ROOT=new URL('./',self.location.href);
 const PREFIX='iec-board-pwa-shell:'+ROOT.pathname+':';
-const CACHE=PREFIX+'v31-slow-gas-startup';
-const FILES=['./','./index.html','./styles.css','./config.js','./app.js','./entry-session.js','./settings-menu.js','./manifest.webmanifest'];
+const CACHE=PREFIX+'v3-android-notification';
+const FILES=['./','./index.html','./styles.css','./config.js','./app.js','./manifest.webmanifest'];
 const PUBLIC_URLS=FILES.map(path=>new URL(path,ROOT).href);
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(PUBLIC_URLS)).then(()=>self.skipWaiting()));
